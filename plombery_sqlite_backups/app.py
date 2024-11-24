00a377a72efa88ea9b79349f644d1a0f8e06093d
Sebastian Strobl <sebastian.strobl@gmx.de>
2024-11-24 18:52:01 +0100

    added cli

--- plombery_sqlite_backups/app.py
+++ plombery_sqlite_backups/app.py
@@ -1,6 +1,9 @@
+import uvicorn
+from plombery import get_app
 from plombery_sqlite_backups import pipeline_full_backup, pipeline_inc_backup
 
-if __name__ == "__main__":
-    import uvicorn
-
+def cli():
     uvicorn.run("plombery:get_app", reload=True, factory=True)
+
+if __name__ == "__main__":
+    cli()
