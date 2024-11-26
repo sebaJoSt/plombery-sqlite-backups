6836ca6b359a9ca12516d2c201adcafd0cfb005d
Sebastian Strobl <sebastian.strobl@gmx.de>
2024-11-26 12:36:36 +0100

    added freeze_support, makes it work when packed with pyinstaller and run on windows machine

--- plombery_sqlite_backups/app.py
+++ plombery_sqlite_backups/app.py
@@ -1,7 +1,9 @@
 import uvicorn
+import multiprocessing
 from plombery_sqlite_backups import pipeline_full_backup, pipeline_inc_backup
 
 def main():
+    multiprocessing.freeze_support()
     uvicorn.run("plombery:get_app", reload=True, factory=True)
 
 if __name__ == "__main__":
