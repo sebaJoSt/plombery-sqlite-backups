95b7184f95d47e865088b6116d6f75fe607ee0df
Sebastian Strobl <sebastian.strobl@gmx.de>
2024-11-27 18:23:36 +0100

    added skipFullBackups argument

--- plombery_sqlite_backups/app.py
+++ plombery_sqlite_backups/app.py
@@ -5,14 +5,16 @@ from plombery import _Plombery
 from plombery_sqlite_backups import pipeline_full_backup
 
 @click.command()
+@click.argument("skipFullBackups", required=False)
 @click.option("--backupDir", "-b", default=os.path.join(os.path.expanduser("~"), "Documents/Loggbok/database backups/"),type=click.Path(exists=True), required=False, help="Backup directory")
 @click.option("--sqliteFile", "-s", default=os.path.join(os.path.expanduser("~"), "Documents/Loggbok/database.sqlite"),type=click.Path(exists=True), required=False, help="Path to SQLite database file")
-def cli(backupdir: str, sqlitefile: str) -> None:
+def cli(skipfullbackups: str, backupdir: str, sqlitefile: str) -> None:
     
     pipeline_full_backup.init(backupdir, sqlitefile)
     
     app = _Plombery()
-    app.register_pipeline(pipeline_full_backup.create_pipeline())
+    if skipfullbackups != "skipFullBackups":
+        app.register_pipeline(pipeline_full_backup.create_pipeline())
     
     uvicorn.run(app)
     
