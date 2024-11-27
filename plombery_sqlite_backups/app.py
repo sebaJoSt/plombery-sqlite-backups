02c8088593680ab3074e7569cf37335138fa1092
Sebastian Strobl <sebastian.strobl@gmx.de>
2024-11-27 02:14:44 +0100

    readd cli

--- plombery_sqlite_backups/app.py
+++ plombery_sqlite_backups/app.py
@@ -1,10 +1,56 @@
+from datetime import datetime
+from random import randint
 import uvicorn
-import multiprocessing
+from plombery import Pipeline, _Plombery, get_logger, Trigger, task
 from plombery_sqlite_backups import pipeline_full_backup, pipeline_inc_backup
+from apscheduler.triggers.interval import IntervalTrigger
 
-def main():
-    multiprocessing.freeze_support()
-    uvicorn.run("plombery:get_app", reload=True, factory=True)
+def cli():
+    app = _Plombery()
+    app.register_pipeline(sales_pipeline)
+    uvicorn.run(app)
+    #uvicorn.run("plombery:get_app", reload=True, factory=True)
+
+@task
+async def fetch_raw_sales_data():
+    # using MarioPype logger your logs will be stored
+    # and accessible on the web UI
+    logger = get_logger()
+
+    logger.debug("Fetching sales data...")
+
+    sales = [
+        {
+            "price": randint(1, 1000),
+            "store_id": randint(1, 10),
+            "date": datetime.today(),
+            "sku": randint(1, 50),
+        }
+        for _ in range(50)
+    ]
+
+    logger.info("Fetched %s sales data rows", len(sales))
+
+    # Return the results of your task to have it stored
+    # and accessible on the web UI
+    return sales
+
+sales_pipeline = Pipeline(
+    id="sales_pipeline",
+    tasks = [fetch_raw_sales_data],
+    triggers = [
+        Trigger(
+            id="daily",
+            name="Daily",
+            description="Run the pipeline every day",
+            schedule=IntervalTrigger(days=1),
+        )
+    ],
+)
+
+
+
 
 if __name__ == "__main__":
-    main()
+    cli()
+
